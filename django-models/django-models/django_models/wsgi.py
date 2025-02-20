06a0aa7d4b74d6b087f1ad85d1c5cff0006552e7
Onserio Ogeto <onserioogeto@gmail.com>
2025-02-20 13:42:46 +0300

    restarting task

--- django-models/django-models/django_models/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
-application = get_wsgi_application()
