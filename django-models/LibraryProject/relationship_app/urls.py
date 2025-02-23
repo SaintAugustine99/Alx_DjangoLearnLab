43a4286cef54dfd01843ee990b36eb648d0e7621
Onserio Ogeto <onserioogeto@gmail.com>
2025-02-23 20:08:30 +0300

    fixing errors

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import register, CustomLoginView, CustomLogoutView
+from .views import register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # Authentication URLs
     path('register/', register, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 # relationship_app/views.py
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
