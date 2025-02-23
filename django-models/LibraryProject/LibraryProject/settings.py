acaea395a2295629182953e76a5d42c6553268bb
Onserio Ogeto <onserioogeto@gmail.com>
2025-02-23 20:36:51 +0300

    task debugging

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookshelf',
     'relationship_app', 
-    'django.contrib.auth',
 ]
 
 MIDDLEWARE = [
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from views import register
 from django.contrib.auth.views import LoginView, LogoutView
+views.register
 
 urlpatterns = [
     path('register/', register, name='register'),
