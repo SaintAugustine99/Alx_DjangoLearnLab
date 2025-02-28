d836d6038cd4779d67a9c4787eb7136d17e8a562
Onserio Ogeto <onserioogeto@gmail.com>
2025-02-28 13:41:59 +0300

    task 2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,5 @@
-# Create your models here.
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -16,6 +15,31 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-    
+
 class CustomUserManager(BaseUserManager):
-    ", "create_user", "create_superuser"
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can View Model'),
+            ('can_create', 'Can Create Model'),
+            ('can_edit', 'Can Edit Model'),
+            ('can_delete', 'Can Delete Model'),
+        ]
