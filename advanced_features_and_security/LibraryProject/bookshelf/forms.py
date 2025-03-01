a8239c1de465da4bdece32401d553cd7c103a16c
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-01 21:52:18 +0300

    Implementation of security

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -23,3 +23,19 @@ class BookForm(forms.ModelForm):
         if not author:
             raise forms.ValidationError('This field is required.')
         return author
+    
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if not title:
+            raise forms.ValidationError('This field is required.')
+        return title
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if not description:
+            raise forms.ValidationError('This field is required.')
+        return description
