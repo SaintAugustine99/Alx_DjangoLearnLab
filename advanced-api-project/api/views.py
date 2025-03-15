3f49908276ec8d642f9284a43aacc9b5e1ee6dea
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-15 18:52:56 +0300

    Building of Custom Views and Generic Views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
-from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class AuthorList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
