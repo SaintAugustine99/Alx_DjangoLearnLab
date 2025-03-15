27b3b45e88e787fbebc4118db5e657903ec452e9
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-15 19:59:56 +0300

    Writing unit tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Book
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 class BookAPITestCase(APITestCase):
     def setUp(self):
@@ -10,6 +12,13 @@ class BookAPITestCase(APITestCase):
         self.book = Book.objects.create(**self.book_data)
         self.list_url = reverse("book-list")  # Assumes DRF router naming
         self.detail_url = reverse("book-detail", kwargs={"pk": self.book.id})
+         self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpassword123'
+        )
+        self.client.login(username='testuser', password='testpassword123')
+        
 
     def test_create_book(self):
         response = self.client.post(self.list_url, self.book_data)
