85dbe9b3b10419f9f667cb8d0fa503a9ac7b6c77
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-25 13:43:23 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -222,4 +222,5 @@ def get_following(request):
     return Response(serializer.data)
 
 #generics.GenericAPIView", "CustomUser.objects.all() generics.GenericAPIView", "CustomUser.objects.all()
-#Post.objects.filter(author__in=following_users).order_by"]
+#Post.objects.filter(author__in=following_users).order_by"] = "Post.objects.filter(author__in=following_users).order_by
+#Post.objects.filter(author__in=following_users) | Q(author=request.user)"] = "Post.objects.filter(author__in=following_users) | Q(author=request.user)
