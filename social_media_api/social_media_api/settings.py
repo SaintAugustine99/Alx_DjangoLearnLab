133e04e697418e4f3ffd94f7ba2bcabf97f2d528
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-26 10:27:57 +0300

    adding Deployment functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9gz0a@j)cfq*h=cf=o&e7a&f8s00rr89-+!5@n7v(9y%f=b@%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
